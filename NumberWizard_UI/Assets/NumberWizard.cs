118f1f05462e4d64057d8f37d39b5c33d90ee41f
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-04-10 10:32:35 -0700

    completed

---
+++ NumberWizard_UI/Assets/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+
+	public int maxGuessesAllowed;
+	public Text text; //requires UnityEngine.UI
+
+
+	void Start () {
+		StartGame ();
+	}
+
+
+	void StartGame() 
+	{
+		max = 1000;
+		min = 1;
+		maxGuessesAllowed = 5;
+		NextGuess ();
+	}
+
+
+	public void GuessLower (){
+		max = guess;
+		NextGuess ();
+	}
+
+	public void GuessHigher () {
+		min = guess;
+		NextGuess ();
+	}
+
+	void NextGuess () {
+		guess = Random.Range(min, max + 1);
+		text.text = guess.ToString();
+		maxGuessesAllowed--;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel ("Win");
+		}
+	}
+}
