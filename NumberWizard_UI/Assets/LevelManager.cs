e83f887664bc101173ba1bcd2153827045a053ae
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-04-10 09:34:00 -0700

    up to lec51

--- NumberWizard_UI/Assets/LevelManager.cs
+++ NumberWizard_UI/Assets/LevelManager.cs
@@ -6,9 +6,11 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
 		Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel (name);
 	}
 
 	public void QuitRequest () {
 		Debug.Log ("I want to quit!");
+		Application.Quit ();
 	}
 }
